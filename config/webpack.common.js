6c7154a795e4d242f89eb05604d3a5064b1bb45b
przemyslaw zakoscielny <pzakosci@akamai.com>
2016-10-11 12:17:26 +0200

    cleaning local setup

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -21,7 +21,8 @@ const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin')
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'Parma Pizza Admin',
+  description: 'Parma Pizza Admin Panel',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
